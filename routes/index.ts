f1513a13e4566103c7517d68230ef94fdb13ec16
ekrzeptowski <contact@ekrzeptowski.pl>
2024-12-25 13:46:27 +0100

    Replace JS code with TS

---
+++ routes/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+const router = express.Router();
+
+/* GET home page. */
+export default router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
